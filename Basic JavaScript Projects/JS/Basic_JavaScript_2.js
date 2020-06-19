850acb40b9dc3f89f4e8911a8f74d4851a564afb
jdwaldman55 <jdwaldman55@gmail.com>
2020-06-19 14:00:43 -0400

    Add files via upload
    Add HTML and JavaScript basic 2 files

--- Basic JavaScript Projects/JS/Basic_JavaScript_2.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -4,3 +4,13 @@ document.getElementById("Add").innerHTML = x;
 function myfunction(a, b) {
     return a * b;
 }
+
+function myfunction2() {
+    document.getElementById("Hello").innerHTML = "Hello";
+}
+
+function myfunction3() {
+    var sentence = "Great job, ";
+    sentence += " You clicked me!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
