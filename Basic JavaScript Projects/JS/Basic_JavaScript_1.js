c50b195f718d3ea4ab60b970f0fdde22810957d9
jdwaldman55 <jdwaldman55@gmail.com>
2020-06-19 11:08:53 -0400

    Add Basic Javascript folder
    Add Basic Javascript folder with HTML and JavaScript documents

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,18 @@
+window.alert("This is a window alert");   //This add a window alert
+
+document.write("This is the document write <br>");  //This is a document wwrite
+
+var B = ("This is my,"       //Concatenated string assigned to a variable
++ " Concatenated string. <br>")
+
+document.write(B)
+
+document.write(5*5)     //Statement containing an expression 
+
+var Family = "The Johnsons", Dad="Bob", Mom="Jane", Son="Tim", Daughter="Mary"
+
+var Dad = Dad.fontcolor("green");      //Turns variable 'Dad' font color green
+var Mom = Mom.fontcolor("blue");       //Turns variable 'Mom' font color blue               
+var Son = Son.fontcolor("orange");     //Turns variable 'Son' font color orange 
+var Daughter = Daughter.fontcolor("pink");   //Turns variable 'Daughter' font color pink 
+
